b167a26d09775394de259c7e38dd5412191dbc86
Daniil Dubrovsky <stillcxld@icloud.com>
2023-03-24 04:24:21 +0400

    testing branch commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,19 +1,49 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import  static org.testng.Assert.assertEquals;
 
 class CashbackHackServiceTest {
-    @Test
-    void shouldReturn10IfAmountIs2000() {
+    @org.testng.annotations.Test
+    public void testRemainWithCashback() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1900;
 
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
 
-        Assertions.assertEquals(expected, actual);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWhenBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWhen1BelowBounary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWhen1AboveBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
     }
 }
